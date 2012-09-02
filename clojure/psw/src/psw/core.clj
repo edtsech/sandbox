4b13f10ae798db49937109e07fe27ae20a35e6e4
Edward Tsech <edtsech@gmail.com>
2012-09-02 21:07:50 +0200

    Fix indentation,

--- clojure/psw/src/psw/core.clj
+++ clojure/psw/src/psw/core.clj
@@ -3,11 +3,11 @@
 (defn partition-starts-with
   [f coll]
   (lazy-seq
-   (when-let [s (seq coll)]
-     (let [fst (first s)
-           fv (f fst)
-           run (cons fst (take-while (if fv
-                                      #(not= fv (f %))
-                                      #(= fv (f %)))
-                                     (rest s)))]
-       (cons run (partition-starts-with f (drop (count run) s)))))))
+    (when-let [s (seq coll)]
+      (let [fst (first s)
+            fv (f fst)
+            run (cons fst (take-while (if fv
+                                        #(not= fv (f %))
+                                        #(= fv (f %)))
+                                      (rest s)))]
+        (cons run (partition-starts-with f (drop (count run) s)))))))
